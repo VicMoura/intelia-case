38beb3b358c8ad6186bf1dfce068eeeb8b7c94e6
Victor Moura <vicmourator@gmail.com.br>
2024-11-15 22:29:28 -0300

    feat(KAN-4) : Adicionando v-mask e router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from '@/plugins/vuetify'
+import router from "./router/router.js";
+import VMask from 'v-mask';
+
 
 Vue.config.productionTip = false
+Vue.use(VMask);
 
 new Vue({
+  vuetify,
+  router,
   render: h => h(App),
 }).$mount('#app')
